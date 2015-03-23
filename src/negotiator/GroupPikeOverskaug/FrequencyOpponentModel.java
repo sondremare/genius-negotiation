8a9f5d3140d050e7887a5f2b32c747fc8111a2b5
markustp <markustpike@gmail.com>
2015-03-23 16:13:05 +0100

    Hopefuly fixed FrequencyOpponentModel

--- src/negotiator/GroupPikeOverskaug/FrequencyOpponentModel.java
+++ src/negotiator/GroupPikeOverskaug/FrequencyOpponentModel.java
@@ -2,9 +2,12 @@ package negotiator.GroupPikeOverskaug;
 
 import negotiator.Bid;
 import negotiator.issue.Issue;
+import negotiator.issue.IssueDiscrete;
+import negotiator.issue.ValueDiscrete;
 import negotiator.utility.UtilitySpace;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Markus on 23.03.2015.
@@ -12,21 +15,30 @@ import java.util.HashMap;
 public class FrequencyOpponentModel
 {
     UtilitySpace utilitySpace;
-    HashMap<Issue, Integer> frequencyMap = new HashMap<Issue, Integer>();
+    HashMap<Issue, HashMap<Integer , Integer>> frequencyMap = new HashMap<Issue, HashMap<Integer , Integer>>();
     double totalCount = 0;
 
     public FrequencyOpponentModel(UtilitySpace utilitySpace) {
         this.utilitySpace = utilitySpace;
-        for (Issue issues : utilitySpace.getDomain().getIssues()) {
-            frequencyMap.put(issues, 0);
+        for (Issue issue : utilitySpace.getDomain().getIssues()) {
+            List<ValueDiscrete> values = ((IssueDiscrete)issue).getValues();
+            HashMap<Integer, Integer> valueMap = new HashMap<Integer, Integer>();
+            for (int valueIndex = 0; valueIndex < values.size(); valueIndex++) {
+                valueMap.put(valueIndex, 0);
+            }
+            frequencyMap.put(issue, valueMap);
         }
 
     }
 
     public void updateModel(Bid bid) {
         for (Issue issue : bid.getIssues()) {
-            frequencyMap.put(issue, frequencyMap.get(issue) + 1);
-            totalCount++;
+            List<ValueDiscrete> values = ((IssueDiscrete)issue).getValues();
+            HashMap<Integer, Integer> valueMap = frequencyMap.get(issue);
+            for (int valueIndex = 0; valueIndex < values.size(); valueIndex++) {
+                valueMap.put(valueIndex, valueMap.get(valueIndex) + 1);
+                totalCount++;
+            }
         }
     }
 
@@ -34,9 +46,15 @@ public class FrequencyOpponentModel
     {
         double utility = 0;
         for (Issue issue : bid.getIssues()) {
-            double weight = frequencyMap.get(issue) / totalCount;
-            utility += weight;
+            List<ValueDiscrete> values = ((IssueDiscrete)issue).getValues();
+            HashMap<Integer, Integer> valueMap = frequencyMap.get(issue);
+            for (int valueIndex = 0; valueIndex < values.size(); valueIndex++) {
+                double weight = valueMap.get(valueIndex) / totalCount;
+                utility += weight;
+            }
+
         }
+        return utility;
     }
 
 
