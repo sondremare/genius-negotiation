c8b5ceead90129d37f44cd87f7b7e3cf28810c78
markustp <markustpike@gmail.com>
2015-03-23 16:54:55 +0100

    tried to fix FrequencyOpponentModel.. again.. still does not work correctly

--- src/negotiator/GroupPikeOverskaug/FrequencyOpponentModel.java
+++ src/negotiator/GroupPikeOverskaug/FrequencyOpponentModel.java
@@ -3,6 +3,7 @@ package negotiator.GroupPikeOverskaug;
 import negotiator.Bid;
 import negotiator.issue.Issue;
 import negotiator.issue.IssueDiscrete;
+import negotiator.issue.Value;
 import negotiator.issue.ValueDiscrete;
 import negotiator.utility.UtilitySpace;
 
@@ -15,46 +16,53 @@ import java.util.List;
 public class FrequencyOpponentModel
 {
     UtilitySpace utilitySpace;
-    HashMap<Issue, HashMap<Integer , Integer>> frequencyMap = new HashMap<Issue, HashMap<Integer , Integer>>();
+    HashMap<Issue, HashMap<Value , Integer>> frequencyMap = new HashMap<Issue, HashMap<Value , Integer>>();
     double totalCount = 0;
 
     public FrequencyOpponentModel(UtilitySpace utilitySpace) {
         this.utilitySpace = utilitySpace;
         for (Issue issue : utilitySpace.getDomain().getIssues()) {
             List<ValueDiscrete> values = ((IssueDiscrete)issue).getValues();
-            HashMap<Integer, Integer> valueMap = new HashMap<Integer, Integer>();
-            for (int valueIndex = 0; valueIndex < values.size(); valueIndex++) {
-                valueMap.put(valueIndex, 0);
+            HashMap<Value, Integer> valueMap = new HashMap<Value, Integer>();
+            for (Value value : values) {
+                valueMap.put(value, 0);
             }
             frequencyMap.put(issue, valueMap);
         }
     }
 
     public void updateModel(Bid bid) {
-        for (Issue issue : bid.getIssues()) {
-            List<ValueDiscrete> values = ((IssueDiscrete)issue).getValues();
-            System.out.println("Issue: " + issue.getName() + " has values: " + values);
-            HashMap<Integer, Integer> valueMap = frequencyMap.get(issue);
-            for (int valueIndex = 0; valueIndex < values.size(); valueIndex++) {
-                valueMap.put(valueIndex, valueMap.get(valueIndex) + 1);
-                totalCount++;
-            }
+        for (int issueIndex = 0; issueIndex < bid.getIssues().size(); issueIndex++) {
+            Issue issue = bid.getIssues().get(issueIndex);
+            Value issueValue = findValue(bid, issueIndex);
+
+            HashMap<Value, Integer> valueMap = frequencyMap.get(issue);valueMap.put(issueValue, valueMap.get(issueValue) + 1);
+            totalCount++;
         }
     }
 
-    public double getUtility(Bid bid)
-    {
+    public double getUtility(Bid bid) {
         double utility = 0;
-        for (Issue issue : bid.getIssues()) {
-            List<ValueDiscrete> values = ((IssueDiscrete)issue).getValues();
-            HashMap<Integer, Integer> valueMap = frequencyMap.get(issue);
-            for (int valueIndex = 0; valueIndex < values.size(); valueIndex++) {
-                double weight = valueMap.get(valueIndex) / totalCount;
-                utility += weight;
-            }
+        for (int issueIndex = 0; issueIndex < bid.getIssues().size(); issueIndex++) {
+            Issue issue = bid.getIssues().get(issueIndex);
+            Value issueValue = findValue(bid, issueIndex);
 
+            HashMap<Value, Integer> valueMap = frequencyMap.get(issue);
+            double weight = valueMap.get(issueValue) / totalCount;
+            utility += weight;
         }
+        System.out.println(utility);
         return utility;
+    }
+
+    private Value findValue(Bid bid, int issueIndex) {
+        Value issueValue = null;
+        try {
+            issueValue = bid.getValue(issueIndex+1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return issueValue;
     }
 
 
